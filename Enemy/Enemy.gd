2ebc0e54155a4c950f5d09354b3cab01aca984c9
HKtwentyfourteen <102879869+HKtwentyfourteen@users.noreply.github.com>
2022-05-29 18:18:41 -0400

    movement and killing
    I made units move. I also prompted them to attack each other on contact repeatedly until one or both of them are dead. Still needs a spawner. Perhaps allowances for multiple rows should be made.

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -2,16 +2,47 @@ extends KinematicBody
 
 var shoot_distance:float = 70
 var close_soldier
+var b = 1
+var hp = 30
+var attack = 3
+var move = true
+var speed = Vector3(-10,0,0)
+var die = false
+var enemy = 1
+var wait = 10
 
 
 func _physics_process(delta):
 
-	var all_soldiers = get_tree().get_nodes_in_group("soldier")
+	var all_soldier = get_tree().get_nodes_in_group("soldier")
+	
+	if die == true:
+		queue_free()
+	
+	if 1 == 0:#sealed away for later incase it becomes useful
+		for soldier in all_soldier:
+			
+			var distance = translation.distance_to(soldier.translation)
+			
+			if distance < shoot_distance:
+				shoot_distance = distance
+				close_soldier = soldier
+	
+	if move == true:
+		move_and_slide(speed)
+	else:
+		enemy.hp -= attack
+		if enemy.hp <= 0:
+			move = true
+	
+	if hp <= 0:
+		die = true
 
-	for soldier in all_soldiers:
-		
-		var distance = translation.distance_to(soldier.translation)
-		
-		if distance < shoot_distance:
-			shoot_distance = distance
-			close_soldier = soldier
+
+func _on_Area_body_entered(body):#assign target
+	if body.is_in_group("soldier"):
+		body.hp -= 10
+		enemy = body
+		move = false
+
+
--- Soldier/Soldier.gd
+++ Soldier/Soldier.gd
@@ -2,16 +2,49 @@ extends KinematicBody
 
 var shoot_distance:float = 70
 var close_enemy
+var b = 1
+var hp = 30
+var attack = 4
+var move = true
+var speed = Vector3(10,0,0)
+var die = false
+var enemy = 1
+var wait = 10
 
 
 func _physics_process(delta):
 
 	var all_enemy = get_tree().get_nodes_in_group("enemy")
+	
+	if die == true:
+		queue_free()
+	
+	if 1 == 0:#sealed away for later incase it becomes useful
+		for enemy in all_enemy:
+			
+			var distance = translation.distance_to(enemy.translation)
+			
+			if distance < shoot_distance:
+				shoot_distance = distance
+				close_enemy = enemy
+	
+	if move == true:#movement
+		move_and_slide(speed)
+	else:#fighting
+		enemy.hp -= attack
+		print(enemy.hp)
+		if enemy.hp <= 0:#continuing after fight
+			move = true
+	
+	if hp <= 0:
+		die = true
 
-	for enemy in all_enemy:
-		
-		var distance = translation.distance_to(enemy.translation)
-		
-		if distance < shoot_distance:
-			shoot_distance = distance
-			close_enemy = enemy
+
+func _on_Area_body_entered(body):#assign target
+	if body.is_in_group("enemy"):
+		body.hp -= 10
+		enemy = body
+		move = false
+	
+
+
